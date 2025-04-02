8ee98b864f1d2c7d52f75a04a84d7a4934d8fe89
Abdess <lmouatassime@gmail.com>
2025-04-02 17:10:35 +0000

    rverting

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/abdess-portfolio">
-    <App />
-  </BrowserRouter>  
-);
+createRoot(document.getElementById("root")!).render(<App />);
+
